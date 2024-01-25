56cdf09f40821910d0fbc3de37356bdd78b2de4e
Harshinilol <146374692+Harshinilol@users.noreply.github.com>
2024-01-25 23:45:02 +0530

    Math.py

--- day01/Math.py
+++ day01/Math.py
@@ -1,3 +1,11 @@
 # write a function to implement addition
-def add(a,b):
-    pass
+def add(a, b):
+    sum = a + b
+    return sum
+
+num1 = int(input())
+num2 = int(input())
+print(add(num1, num2))
+
+
+
